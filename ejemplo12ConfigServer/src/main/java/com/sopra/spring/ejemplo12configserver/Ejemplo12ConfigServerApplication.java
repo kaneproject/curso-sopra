f2d0f81dbc931f1be7464374c7e0c7b364b2101a
Ruben Gomez Garcia <rubengomez78+sopra@gmail.com>
2019-07-15 11:08:43 +0200

    Agregado ejemplo de ConfigServer

---
+++ ejemplo12ConfigServer/src/main/java/com/sopra/spring/ejemplo12configserver/Ejemplo12ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.sopra.spring.ejemplo12configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Ejemplo12ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo12ConfigServerApplication.class, args);
+	}
+
+}
---
+++ ejemplo12ConfigServer/src/test/java/com/sopra/spring/ejemplo12configserver/Ejemplo12ConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sopra.spring.ejemplo12configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ejemplo12ConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
