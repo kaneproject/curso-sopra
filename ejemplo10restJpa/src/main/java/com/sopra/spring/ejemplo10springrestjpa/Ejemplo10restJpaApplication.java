0b01f7a3ee16600c81685688cd6db44158d969d2
Ruben Gomez Garcia <rubengomez78+sopra@gmail.com>
2019-07-15 11:06:45 +0200

    Agregado ejemplo10 a Eureka

--- ejemplo10restJpa/src/main/java/com/sopra/spring/ejemplo10springrestjpa/Ejemplo10restJpaApplication.java
+++ ejemplo10restJpa/src/main/java/com/sopra/spring/ejemplo10springrestjpa/Ejemplo10restJpaApplication.java
@@ -2,8 +2,10 @@ package com.sopra.spring.ejemplo10springrestjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Ejemplo10restJpaApplication {
 
 	public static void main(String[] args) {
